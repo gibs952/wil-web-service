7683e7ad236b51b1dc561659ab8b7cf80415e4fc
Muhammad Wildan <wildankhairusunny@gmail.com>
2024-03-04 14:43:58 +0700

    golang

---
+++ Petemuan2/itung/Halo.go
@@ -0,0 +1,16 @@
+package itung
+
+func tambah(a, b int) int {
+	return a + b
+}
+
+func kurang(a, b int) int {
+	return a + b
+}
+
+func kali(a, b int) int {
+	return a * b
+}
+func bagi(a, b int) int {
+	return a / b
+}
---
+++ Petemuan2/test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"Pertemuan2/itung"
+	"fmt"
+)
+
+func main() {
+	hasil := itung.tambah(3, 5)
+
+	fmt.Println("Hasil pertambahan: ", hasil)
+}
